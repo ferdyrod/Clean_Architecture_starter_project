9a9d954254e3e4562844d8a95ab209dd61e87e03
Ferdy Rodriguez <ferdyrodriguez@gmail.com>
2017-06-12 00:14:14 +0200

    fixes presentation module structure

--- presentation/src/androidTest/java/com/ferdyrodriguez/package_Name/presentation/ExampleInstrumentedTest.java
+++ presentation/src/androidTest/java/com/ferdyrodriguez/package_Name/presentation/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.ferdyrodriguez.package_Name.presentation.test", appContext.getPackageName());
+        assertEquals("com.ferdyrodriguez.package_Name.presentation", appContext.getPackageName());
     }
 }
